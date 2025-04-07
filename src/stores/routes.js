b17604402532e2ca86d6433d28692f047095b5be
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2025-04-07 10:45:32 +0200

    Update store for nav management

---
+++ src/stores/routes.js
@@ -0,0 +1,25 @@
+import { computed, ref, shallowRef } from 'vue';
+import PageTemperature from '@/components/pages/PageTemperature.vue';
+import PageWeight from '@/components/pages/PageWeight.vue';
+
+export const routes = [
+  {
+    anchor: 'temp',
+    label: 'Temperature',
+    page: PageTemperature,
+  }, {
+    anchor: 'weight',
+    label: 'Weight',
+    page: PageWeight,
+  }, {
+    anchor: 'test',
+    label: 'Test',
+    page: PageWeight,
+  },
+];
+
+export const curAnchor = ref('temp');
+
+export const currentPage = computed(() => {
+  return routes.find((route) => route.anchor === curAnchor.value)?.page || PageTemperature;
+});
